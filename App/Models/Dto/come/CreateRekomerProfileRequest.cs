7f1961d7babd300c1575e31a9f164f8bb42bca32
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 20:59:37 +0700

    feat(dto): create CreateRekomerProfileRequest

---
+++ App/Models/Dto/come/CreateRekomerProfileRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RekomBackend.App.Models.Dto;
+
+public class CreateRekomerProfileRequest
+{
+   [MinLength(1, ErrorMessage = "The Avatar field is required.")]
+   public string FullName { get; set; } = null!;
+
+   public IFormFile Avatar { get; set; } = null!;
+}
