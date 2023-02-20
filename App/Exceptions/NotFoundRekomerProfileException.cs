21367627779c4e73a8a6247f31acfa3e242efe7e
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-21 00:22:38 +0700

    feat(exception): create NotFoundRekomerProfileException

---
+++ App/Exceptions/NotFoundRekomerProfileException.cs
@@ -0,0 +1,8 @@
+﻿namespace RekomBackend.App.Exceptions;
+
+public class NotFoundRekomerProfileException : Exception
+{
+   public NotFoundRekomerProfileException(string? message = null!) : base(message)
+   {
+   }
+}
