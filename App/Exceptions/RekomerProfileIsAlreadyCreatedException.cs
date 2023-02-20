a04d4b8196d66ef0306bc5b423ca259a661cb195
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 23:22:33 +0700

    feat(exception): create RekomerProfileIsAlreadyCreatedException

---
+++ App/Exceptions/RekomerProfileIsAlreadyCreatedException.cs
@@ -0,0 +1,8 @@
+﻿namespace RekomBackend.App.Exceptions;
+
+public class RekomerProfileIsAlreadyCreatedException : Exception
+{
+   public RekomerProfileIsAlreadyCreatedException(string? message = null) : base(message)
+   {
+   }
+}
