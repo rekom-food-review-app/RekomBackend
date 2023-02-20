90f49abd365b986bf1683940ef8d5a8d961c06b9
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 14:42:38 +0700

    feat(service): add SendEmailToConfirmAccountAsync

---
+++ App/Services/mail/IMailService.cs
@@ -0,0 +1,8 @@
+﻿using RekomBackend.App.Models.Entities;
+
+namespace RekomBackend.App.Services.mail;
+
+public interface IMailService
+{
+   public Task SendEmailToConfirmAccountAsync(string email, string otp);
+}
---
+++ App/Services/mail/MailService.cs
@@ -0,0 +1,23 @@
+﻿using RekomBackend.App.Helpers;
+
+namespace RekomBackend.App.Services.mail;
+
+public class MailService : IMailService
+{
+   private readonly IMailHelper _mailHelper;
+
+   public MailService(IMailHelper mailHelper)
+   {
+      _mailHelper = mailHelper;
+   }
+
+   public async Task SendEmailToConfirmAccountAsync(string emailAddress, string otp)
+   {
+      var mail = new MailDataBuilder();
+      mail.SendTo(emailAddress);
+      mail.SetBody($"hi, bitch {otp}");
+      mail.SetSubject("yeahhhh");
+      
+      await _mailHelper.SendEmailAsync(mail);
+   }
+}
