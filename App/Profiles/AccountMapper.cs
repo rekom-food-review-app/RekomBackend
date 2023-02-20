cc3ba953b5981bdc760ddd344e7ec926fb323293
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 10:51:02 +0700

    feat(profile): create mapper <RegisterWithEmailRequest, Account>

---
+++ App/Profiles/AccountMapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using RekomBackend.App.Models.Dto;
+using RekomBackend.App.Models.Entities;
+
+namespace RekomBackend.App.Profiles;
+
+public class AccountMapper : Profile
+{
+   public AccountMapper()
+   {
+      CreateMap<RegisterWithEmailRequest, Account>()
+      .ForMember(
+         dest => dest.Id,
+         opt => opt.MapFrom(src => Guid.NewGuid().ToString())
+      )
+      .ForMember(
+         dest => dest.Username,
+         opt => opt.MapFrom(src => src.Username)
+      )
+      .ForMember(
+         dest => dest.Email,
+         opt => opt.MapFrom(src => src.Email)
+      )
+      .ForMember(
+         dest => dest.Role,
+         opt => opt.MapFrom(src => src.Role)
+      );
+   }
+}
