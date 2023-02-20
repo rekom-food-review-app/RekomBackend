4b2424f4c9a57bbf7bbe51d193d99a0febb9d137
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 09:38:22 +0700

    feat(dto): create RegisterWithEmailRequest

---
+++ App/Models/Dto/come/RegisterWithEmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RekomBackend.App.Models.Dto;
+
+public class RegisterWithEmailRequest
+{
+   [MinLength(1, ErrorMessage = "required")]
+   public string Username { get; set; } = null!;
+
+   [EmailAddress]
+   public string Email { get; set; } = null!;
+
+   [MinLength(6, ErrorMessage = "at least 6 characters")]
+   public string Password { get; set; } = null!;
+}
