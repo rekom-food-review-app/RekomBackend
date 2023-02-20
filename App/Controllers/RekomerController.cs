12267f9c00182ee23cd05f97fb62bb59ac9876e7
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-20 23:23:19 +0700

    feat: CreateProfile

---
+++ App/Controllers/RekomerController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RekomBackend.App.Exceptions;
+using RekomBackend.App.Models.Dto;
+using RekomBackend.App.Services;
+
+namespace RekomBackend.App.Controllers;
+
+[ApiController]
+[Route("rekomers")]
+public class RekomerController : ControllerBase
+{
+   private readonly IRekomerService _rekomerService;
+
+   public RekomerController(IRekomerService rekomerService)
+   {
+      _rekomerService = rekomerService;
+   }
+   
+   [HttpPost("me/profile"), Authorize(Roles = "Rekomer")]
+   public async Task<IActionResult> CreateProfile([FromForm] CreateRekomerProfileRequest createRequest)
+   {
+      try
+      {
+         await _rekomerService.CreateProfileAsync(createRequest);
+      
+         return Ok(new
+         {
+            code = "CPS",
+            message = "Create Profile Successfully."
+         });
+      }
+      catch (InvalidAccessTokenException e)
+      {
+         return Unauthorized();
+      }
+      catch (RekomerProfileIsAlreadyCreatedException e)
+      {
+         return BadRequest(new
+         {
+            code = "PAC",
+            message = "Your Profile Is Already Created."
+         });
+      }
+   }
+}
