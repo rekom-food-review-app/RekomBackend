d1ee81034f709d39a6da90b11702eff0d2d11793
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-21 10:33:46 +0700

    refactor: remove rekomer-profile namespace

--- App/Services/rekomer-profile/IRekomerService.cs
+++
@@ -1,8 +0,0 @@
-﻿using RekomBackend.App.Models.Dto;
-
-namespace RekomBackend.App.Services;
-
-public interface IRekomerService
-{
-   public Task CreateProfileAsync(CreateRekomerProfileRequest createRequest);
-}
