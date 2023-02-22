c8b9a0779c555a50be68e47e8dc456d57ceeb1a0
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 00:51:56 +0700

    feat: add Authorize

--- App/Controllers/RekomerSideControllers/RekomerRestaurantController.cs
+++ App/Controllers/RekomerSideControllers/RekomerRestaurantController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RekomBackend.App.Services.RekomerSideServices;
 
 namespace RekomBackend.App.Controllers.RekomerSideControllers;
 
 [ApiController]
 [Route("rekomer-side/restaurants")]
+[Authorize(Roles = "Rekomer")]
 public class RekomerRestaurantController : ControllerBase
 {
    private readonly IRekomerRestaurantService _restaurantService;
