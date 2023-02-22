187bebc4f64edea83996a2fb236e08edde981d7b
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 03:11:38 +0700

    feat(dto): create RekomerReviewMediaResponseDto

---
+++ App/Models/Dto/review-media-dto/RekomerReviewMediaResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RekomBackend.App.Models.Dto;
+
+public class RekomerReviewMediaResponseDto
+{
+   public string MediaUrl { get; set; } = null!;
+
+   public string Type { get; set; } = null!;
+}
