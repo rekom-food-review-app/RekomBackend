797fa8265861d2b5deb08741c8b73fef449188ba
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 03:12:28 +0700

    feat(dto): create RekomerReviewCardResponseDto

---
+++ App/Models/Dto/review-dto/RekomerReviewCardResponseDto.cs
@@ -0,0 +1,46 @@
+﻿namespace RekomBackend.App.Models.Dto;
+
+public class RekomerReviewCardResponseDto
+{
+   #region Rekomer
+
+   public string RekomerId { get; set; } = null!;
+
+   public string RekomerName { get; set; } = null!;
+
+   public string RekomerAvatarUrl { get; set; } = null!;
+
+   #endregion
+
+   #region Review
+
+   public string ReviewId { get; set; } = null!;
+   
+   public IEnumerable<RekomerReviewMediaResponseDto> ReviewMedias { get; set; } = null!;
+
+   public string ReviewContent { get; set; } = null!;
+
+   #endregion
+
+   #region Reaction
+
+   public int AmountDislike { get; set; }
+   
+   public int AmountLike { get; set; }
+   
+   public int AmountHelpful { get; set; }
+
+   public int AmountComment { get; set; }
+
+   public string? MyReaction { get; set; }
+
+   #endregion
+
+   #region Restaurant
+
+   public string RestaurantId { get; set; } = null!;
+
+   public string RestaurantName { get; set; } = null!;
+
+   #endregion
+}
