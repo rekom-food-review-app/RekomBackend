d801822f541fdd5650611698d1357851ae50c441
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 03:12:50 +0700

    feat(profiel): create ReviewMediaProfile

---
+++ App/Profiles/ReviewMediaProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using RekomBackend.App.Models.Dto;
+using RekomBackend.App.Models.Entities;
+
+namespace RekomBackend.App.Profiles;
+
+public class ReviewMediaProfile : Profile
+{
+   public ReviewMediaProfile()
+   {
+      CreateMap<ReviewMedia, RekomerReviewMediaResponseDto>();
+   }
+}
