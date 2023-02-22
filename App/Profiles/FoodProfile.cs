5d63f13e8b4ab6277f3bb0291d66ce907c9549c8
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 04:59:31 +0700

    feat(profile): create FoodProfile

---
+++ App/Profiles/FoodProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using RekomBackend.App.Models.Dto.RekomerSideDtos;
+using RekomBackend.App.Models.Entities;
+
+namespace RekomBackend.App.Profiles;
+
+public class FoodProfile : Profile
+{
+   public FoodProfile()
+   {
+      CreateMap<Food, RekomerFoodInMenuResponseDto>();
+   }
+}
