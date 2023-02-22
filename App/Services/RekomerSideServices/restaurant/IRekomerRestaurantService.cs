56ee738027019ad4af1e970c2eb204f6be0f42fd
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 00:32:40 +0700

    feat(service): create GetRestaurantProfileByIdAsync

--- App/Services/RekomerSideServices/restaurant/IRekomerRestaurantService.cs
+++ App/Services/RekomerSideServices/restaurant/IRekomerRestaurantService.cs
@@ -1,6 +1,8 @@
-﻿namespace RekomBackend.App.Services.RekomerSideServices;
+﻿using RekomBackend.App.Models.Dto;
+
+namespace RekomBackend.App.Services.RekomerSideServices;
 
 public interface IRekomerRestaurantService
 {
-   
+   public Task<RekomerRestaurantDetailResponseDto?> GetRestaurantProfileByIdAsync(string restaurantId);
 }
--- App/Services/RekomerSideServices/restaurant/RekomerRestaurantService.cs
+++ App/Services/RekomerSideServices/restaurant/RekomerRestaurantService.cs
@@ -1,6 +1,37 @@
-﻿namespace RekomBackend.App.Services.RekomerSideServices;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RekomBackend.App.Models.Dto;
+using RekomBackend.App.Models.Entities;
+using RekomBackend.Database;
+
+namespace RekomBackend.App.Services.RekomerSideServices;
 
 public class RekomerRestaurantService : IRekomerRestaurantService
 {
-   
+   private readonly RekomContext _context;
+   private readonly IMapper _mapper;
+
+   public RekomerRestaurantService(RekomContext context, IMapper mapper)
+   {
+      _context = context;
+      _mapper = mapper;
+   }
+
+   public async Task<RekomerRestaurantDetailResponseDto?> GetRestaurantProfileByIdAsync(string restaurantId)
+   {
+      var restaurant = await _context.Restaurants
+         .Include(res => res.Reviews!)
+         .ThenInclude(rev => rev.Rating!)
+         .Where(res => res.Id == restaurantId)
+         .FirstOrDefaultAsync();
+
+      if (restaurant is null) { return null; }
+      
+      var ratingResult = restaurant.CalculateRatingResult();
+      
+      var restaurantResponse = _mapper.Map<Restaurant, RekomerRestaurantDetailResponseDto>(restaurant);
+      _mapper.Map(ratingResult, restaurantResponse);
+      
+      return restaurantResponse;
+   }
 }
