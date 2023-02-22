388891af127ccb476ff36e76f627448b8788bb5a
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 00:32:17 +0700

    feat: create RestaurantProfile

---
+++ App/Profiles/RestaurantProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RekomBackend.App.Models.Dto;
+using RekomBackend.App.Models.Entities;
+
+namespace RekomBackend.App.Profiles;
+
+public class RestaurantProfile : Profile
+{
+   public RestaurantProfile()
+   {
+      CreateMap<Restaurant, RekomerRestaurantDetailResponseDto>();
+
+      CreateMap<RatingResultDto, RekomerRestaurantDetailResponseDto>();
+   }
+}
