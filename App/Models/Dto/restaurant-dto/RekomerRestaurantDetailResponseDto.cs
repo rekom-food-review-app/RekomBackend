a841f3d1fa4debaa3733564427cd1b29fd609a7e
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-23 00:30:49 +0700

    feat(dto): create RekomerRestaurantDetailResponseDto

---
+++ App/Models/Dto/restaurant-dto/RekomerRestaurantDetailResponseDto.cs
@@ -0,0 +1,24 @@
+﻿namespace RekomBackend.App.Models.Dto;
+
+public class RekomerRestaurantDetailResponseDto : RatingResultDto
+{
+   #region BasicInfo
+
+   public string Id { get; set; } = null!;
+
+   public string Name { get; set; } = null!;
+
+   public string CoverImageUrl { get; set; } = null!;
+
+   public string Description { get; set; } = null!;
+   
+   public string Coordinates { get; set; } = null!;
+
+   #endregion
+
+   #region Award
+
+   // public IEnumerable<Award> Awards { get; set; } = null!;
+
+   #endregion
+}
