d100a813e89b48e0c04233e72b75a614b1d92387
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-25 22:30:40 +0700

    Update RekomerRegisterService.cs

--- App/Services/RekomerSideServices/register/RekomerRegisterService.cs
+++ App/Services/RekomerSideServices/register/RekomerRegisterService.cs
@@ -29,7 +29,8 @@ public class RekomerRegisterService : IRekomerRegisterService
       var account = new Account
       {
          PasswordHash = registerRequest.Password,
-         Role = Role.Rekomer
+         Role = Role.Rekomer,
+         IsConfirmed = false
       };
       _mapper.Map(registerRequest, account);
       account.Rekomer = new Rekomer
