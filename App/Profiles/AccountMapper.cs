4a7a4b8f30acf9d409d64d83ac6ef055642634fe
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-25 15:55:02 +0700

    feat(profile): update <RegisterWithEmailRequest, Account> mapper

--- App/Profiles/AccountMapper.cs
+++ App/Profiles/AccountMapper.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
-using RekomBackend.App.Common.Enums;
+using RekomBackend.App.Dto.RekomerSideDtos;
 using RekomBackend.App.Entities;
-using RekomBackend.App.Models.Dto;
 
 namespace RekomBackend.App.Profiles;
 
@@ -9,22 +8,18 @@ public class AccountMapper : Profile
 {
    public AccountMapper()
    {
-      CreateMap<RegisterWithEmailRequest, Account>()
-      .ForMember(
-         dest => dest.Id,
-         opt => opt.MapFrom(src => Guid.NewGuid().ToString())
-      )
-      .ForMember(
-         dest => dest.Username,
-         opt => opt.MapFrom(src => src.Username)
-      )
-      .ForMember(
-         dest => dest.Email,
-         opt => opt.MapFrom(src => src.Email)
-      )
-      .ForMember(
-         dest => dest.Role,
-         opt => opt.MapFrom(src => (Role)Enum.Parse(typeof(Role), src.Role))
-      );
+      CreateMap<RekomerRegisterEmailRequestDto, Account>()
+         .ForMember(
+            dest => dest.Id,
+            opt => opt.MapFrom(src => Guid.NewGuid().ToString())
+         )
+         .ForMember(
+            dest => dest.Username,
+            opt => opt.MapFrom(src => src.Username.ToLower().Trim())
+         )
+         .ForMember(
+            dest => dest.Email,
+            opt => opt.MapFrom(src => src.Email.ToLower().Trim())
+         );
    }
 }
