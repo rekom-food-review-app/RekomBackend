03b2d5cd49d046a5dc6fa07626fa50bf20a7868d
vuphamlethanh <vuphamlethanh@gmail.com>
2023-02-25 22:30:55 +0700

    Update RekomerRegisterController.cs

--- App/Controllers/RekomerSideControllers/RekomerRegisterController.cs
+++ App/Controllers/RekomerSideControllers/RekomerRegisterController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RekomBackend.App.Dto.RekomerSideDtos;
 using RekomBackend.App.Dto.RekomerSideDtos.Request;
 using RekomBackend.App.Services.RekomerSideServices;
 
