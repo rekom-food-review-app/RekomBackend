fbc38e9cc912caea5cbce9822262f722826592da
vuphamlethanh <vuphamlethanh@gmail.com>
2023-03-02 17:25:12 +0700

    fix: update EntityBase

--- App/Entities/EntityBase.cs
+++ App/Entities/EntityBase.cs
@@ -5,10 +5,10 @@ public abstract class EntityBase
    #region Columns
 
    public string Id { get; set; } = Guid.NewGuid().ToString();
-   
-   public DateTime CreatedAt { get; set; }
-   
-   public DateTime UpdatedAt { get; set; }
+
+   public DateTime CreatedAt { get; set; } = new DateTime();
+
+   public DateTime UpdatedAt { get; set; } = new DateTime();
 
    #endregion
 
