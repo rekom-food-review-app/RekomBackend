1ee1558d0daa0cf576f0c5b77220b08b458a9828
vuphamlethanh <vuphamlethanh@gmail.com>
2023-03-12 23:50:50 +0700

    Update StringHelper.cs

--- App/Helpers/string/StringHelper.cs
+++ App/Helpers/string/StringHelper.cs
@@ -5,8 +5,10 @@ namespace RekomBackend.App.Helpers;
 
 public class StringHelper : IStringHelper
 {
-   public static string ToEnglish(string input)
+   public static string ToEnglish(string? input)
    {
+      if (input is null) { return string.Empty; }
+      
       return new string(input.Normalize(NormalizationForm.FormD)
          .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
          .ToArray());
